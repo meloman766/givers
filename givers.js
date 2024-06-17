0d9ca085cdb59f54a9cdcef07ac2507effdbdde5
meloman766 <pusher766@gmail.com>
2024-06-17 13:55:12 +0300

    Create givers.js

---
+++ givers.js
@@ -0,0 +1,39 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.givers1000 = exports.givers100 = void 0;
+exports.givers100 = [
+    { address: 'EQCfwe95AJDfKuAoP1fBtu-un1yE7Mov-9BXaFM3lrJZwqg_', reward: 100 },
+    { address: 'EQBoATvbIa9vA7y8EUQE4tlsrrt0EhSUK4mndp49V0z7Me3M', reward: 100 },
+    { address: 'EQAV3tsPXau3VJanBw4KCFaMk3l_n3sX8NHZNgICFrR-9EGE', reward: 100 },
+    { address: 'EQAR9DvLZMHo9FAVMHI1vHvL7Fi7jWgjKtUARZ2S_nopQRYz', reward: 100 },
+    { address: 'EQC10L__G2SeEeM2Lw9osGyYxhoIPqJwE-8Pe7728JcmnJzW', reward: 100 },
+    { address: 'EQDZJFkh12kw-zLGqKSGVDf1V2PRzedGZDFDcFml5_0QerST', reward: 100 },
+    { address: 'EQCiLN0gEiZqthGy-dKl4pi4kqWJWjRzR3Jv4jmPOtQHveDN', reward: 100 },
+    { address: 'EQDB8Mo9EviBkg_BxfNv6C2LO_foJRXcgEF41pmQvMvnB9Jn', reward: 100 },
+    { address: 'EQAidDzp6v4oe-vKFWvsV8MQzY-4VaeUFnGM3ImrKIJUIid9', reward: 100 },
+    { address: 'EQAFaPmLLhXveHcw3AYIGDlHbGAbfQWlH45WGf4K4D6DNZxY', reward: 100 }, // 100
+];
+exports.givers1000 = [
+    { address: 'EQBUmN2uinHEFjBymG3SV010oeDABQ_8R-z1PRuV1YLRImL9', reward: 1000 },
+    { address: 'EQC51U_4w6ItbeHvQs0RhuT3cwsytrDaJbG55BU-ZAzUfB4n', reward: 1000 },
+    { address: 'EQBT-Dbj7JkZltw-IVLbep0NTgaEGgSMUMIp4km8mYkPV6I8', reward: 1000 },
+    { address: 'EQD7GQw_BEo9KX7t1WpIa42KeQ3h8f4hVejip453l4qVLdNk', reward: 1000 },
+    { address: 'EQDEjyEOuJU5UggCaW-aN7_NxPF-ZULzcmCEY1AwEmzg5yuy', reward: 1000 },
+    { address: 'EQD5Viou03CU_oQKsyuneGv4Q2scxeLw-pfY4rIoa8sLlYtI', reward: 1000 },
+    { address: 'EQAWSBd3IiRJQiTWr2qjYNgUFCA0u6zSs5H6S3ghOeMp0s-r', reward: 1000 },
+    { address: 'EQCklqxLokjCHkujRzAZMlaGM4JSUHS3IpUhqQ6y8mzD2DiL', reward: 1000 },
+    { address: 'EQAD7ylSr3X0TVUrjtpaaLhEwZYi9ucCAjdhjANF4SLpFcrr', reward: 1000 },
+    { address: 'EQAdugQfgubyVOtzcfn-ca7WEyepnp7BYP5oS0w5A15zIH78', reward: 1000 }, // 1000
+];
+exports.givers10000 = [
+    { address: 'EQAY4LHfgqInZTekX9SDBmbBimShl3OdmTnQGSw82DQBNKAQ', reward: 10000 },
+    { address: 'EQCfPVHTaKwoPOG5C8jSrhi5j82EuhNdKqoirEPvES8igPPN', reward: 10000 },
+    { address: 'EQB6P_39LCqDIcZ600Y6qenlc47a-FtjpReb4rCHGZOt8b2q', reward: 10000 },
+    { address: 'EQDftSo4Mn53jyUFb5bTzmJevxUjK832iy6Tt4eoYIB9M7J5', reward: 10000 },
+    { address: 'EQAO7jXcX-fJJZl-kphbpdhbIDUqcAiYcAr9RvVlFl38Uatt', reward: 10000 },
+    { address: 'EQDavF_H1EBBsQ82HdyvCExFRbFKQ3mpsHMElE0wbim5ehHM', reward: 10000 },
+    { address: 'EQB-Tc3OrKn__jSa9hTPEmbMioOQN08Fvda_85exIB4GPw4g', reward: 10000 },
+    { address: 'EQAl3C0k1R8X9Ms7VG4cUpM3u_lKqfKIgYCLjMDMqaiwXynW', reward: 10000 },
+    { address: 'EQCHODQyANmzftrcaGUOctE_D7XSEZKTepzxXyGLA6dZNDTJ', reward: 10000 },
+    { address: 'EQDCeae9X_DZji0GEUXV8IFX5BPID2AX8zVCxugeH_H1fb9Z', reward: 10000 }, // 10000
+];
